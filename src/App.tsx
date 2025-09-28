fe722d6e20d2443667f991039e83b055b2db4985
manuTech0 <manu024595@gmail.com>
2025-09-28 18:49:07 +0700

    optimization

--- src/App.tsx
+++ src/App.tsx
@@ -69,17 +69,28 @@ function App() {
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="descripton" content="Portofolio Maulana Nurfanoto, Backend Specialist and IT Consultant" />
         <link rel="canonical" href="https://www.manu-tech.my.id/" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Chocolate+Classical+Sans&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sriracha&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          as="font"
+          href="/fonts/chocolate-classical-sans.woff2"
+          type="font/woff2"
+          crossOrigin=""
         />
         <link
           rel="preload"
-          as="style"
-          href="https://fonts.googleapis.com/css2?family=Chocolate+Classical+Sans&family=Poppins:wght@400;700&family=Sriracha&display=swap"
+          as="font"
+          href="/fonts/sriracha.woff2"
+          type="font/woff2"
+          crossOrigin=""
         />
+        <link
+          rel="preload"
+          as="font"
+          href="/fonts/poppins.woff2"
+          type="font/woff2"
+          crossOrigin=""
+        />
+
       </Helmet>
       <div className="lg:px-10 md:px-10 bg-color-gradient fixed top-0 left-0 w-screen h-dvh">
         {!imageLoaded && (
